d6a9c0c72e67a7e3edd0b8f6faa5ae1b2f647fd5
David Marcus <davidmarcus@mdnmac01.local>
2017-08-16 17:06:06 -0400

    Initial Commit

---
+++ Discharge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Discharge
+//
+//  Created by David Marcus on 8/16/17.
+//  Copyright © 2017 MD Now. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
